e32b418d0de008d0b98b4c08509cd0e84933015c
Shivam Tiwari <st472438@gmail.com>
2025-05-26 10:03:56 +0530

    fix bug

--- frontend/src/pages/User/MyTasks.jsx
+++ frontend/src/pages/User/MyTasks.jsx
@@ -74,7 +74,7 @@ const MyTasks = () => {
               dueDate={item.dueDate}
               assignedTo={item.assignedTo?.map((item) => item.profileImageUrl)}
               attachmentCount={item.attachments?.length || 0}
-              completedTodoCount={item.completedTodoCount?.length || 0}
+              completedTodoCount={item.completedTodoCount || 0}
               todoCheckList={item.todoCheckList || []}
               onClick={() => handleClick(item._id)}
             />
